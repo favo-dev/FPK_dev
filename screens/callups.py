5fcf7a247ff0d3fb7156c62e9e8c66b5b731bc3e
favo-dev <favalli403@gmail.com>
2025-10-13 17:15:24 +0200

    Update callups.py

--- screens/callups.py
+++ screens/callups.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from datetime import datetime, timezone
-from supabase import create_client
 from logic.functions import normalize_riders, get_supabase_client
 
 # -------------------------------------------------------------------------------------------
