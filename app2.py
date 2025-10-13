1759cedaffb3f2e90854c7065f41f5126e1b11a5
favo-dev <favalli403@gmail.com>
2025-10-13 18:11:13 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import datetime
+from supabase import create_client
 import streamlit as st
 from logic.functions import hex_to_rgb
 from logic.auth import (
@@ -333,6 +334,7 @@ else:
                         st.stop()
 
                     st.success("Registration successful! Please log in.")
+
 
 
 
