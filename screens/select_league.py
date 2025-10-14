3fbb37cab47c2d9f049aec46733592f13a083972
favo-dev <favalli403@gmail.com>
2025-10-14 15:37:58 +0200

    Update select_league.py

--- screens/select_league.py
+++ screens/select_league.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from supabase import create_client
+from logic.functions import go_to_screen
 
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+
+# --------------------- SUPABASE CLIENT --------------------------------------
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_ANON_KEY = st.secrets["SUPABASE_ANON_KEY"]
+supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+# --------------------- HOME SCREEN ----------------------------------------------------
+
+def home_screen(user):
+  st.write("Siamo al punto giusto")
