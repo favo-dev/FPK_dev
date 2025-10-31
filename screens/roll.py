8c30a0cadf44ef6ddfe59aa3d3ef2e7e533601cf
favo-dev <favalli403@gmail.com>
2025-10-31 11:16:57 +0100

    Update roll.py

--- screens/roll.py
+++ screens/roll.py
@@ -17,10 +17,10 @@ supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 def roll_screen(user):
     st.set_page_config(layout="wide") 
 
-    data = supabase.from_("roll_of_honor_new").select("*").execute().data or []
-    teams = supabase.from_("teams").select("*").execute().data or []
+    data = supabase.from_("roll_of_honor_new").select("*").eq("league", user['league']).execute().data or []
+    teams = supabase.from_("teams").select("*").eq("league", user['league']).execute().data or []
 
-    team_info = {team["ID"]: team for team in teams}
+    team_info = {team["name"]: team for team in teams}
 
     anni = [entry["year"] for entry in data]
     anno_scelto = st.selectbox("Select a year", sorted(anni, reverse=True))
