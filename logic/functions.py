1b51fbed16b3a0980563721ff0c9cde0f5fefd45
favo-dev <favalli403@gmail.com>
2026-02-03 20:21:47 +0100

    Update functions.py

--- logic/functions.py
+++ logic/functions.py
@@ -458,7 +458,7 @@ def sprint_pole(race, category):
 
 # -------------------------------------------------------------------------------------------
 
-def results_exist(race, tag):
+def results_exist(race, tag, user):
     try:
         data = get_results(tag, race["category"], False, user)
         if not data:
