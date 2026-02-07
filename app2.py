6034f2d135b298b03ae50e36047dd375f8762c1a
favo-dev <favalli403@gmail.com>
2026-02-07 14:42:00 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -60,7 +60,7 @@ for k, v in defaults.items():
         st.session_state[k] = v
 st.set_page_config(
     page_title="FPK 1.2.0 'Hakkinen'",
-    page_icon="https://koffsyfgevaannnmjkvl.supabase.co/storage/v1/object/public/icons/FAVLINK_512.png",
+    page_icon="https://koffsyfgevaannnmjkvl.supabase.co/storage/v1/object/sign/figures/FPK26_1500.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNTU1ZWI5ZC03NmZjLTRiMjUtOGIwMC05ZDQ4ZTRhNGNhMDEiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJmaWd1cmVzL0ZQSzI2XzE1MDAucG5nIiwiaWF0IjoxNzcwNDcxNjk1LCJleHAiOjE4MDIwMDc2OTV9.kvU0DuYYFm7V6fwjPh5KLINFZln--9tXJngVFzet9Nw",
     layout="centered",
     initial_sidebar_state="collapsed",
 )
@@ -299,6 +299,7 @@ else:
                         st.error(f"Errore salvataggio su DB: {e}")
                         st.stop()
                     st.success("Registration successful! Please log in.")
+
 
 
 
